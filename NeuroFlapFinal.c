c5138d2be52d93d46cf6017da7ed4674ab2f91be
Eliezer <eliezer.carvalho.11.8a@gmail.com>
2026-03-08 23:39:43 +0000

    main

---
+++ NeuroFlapFinal.c
@@ -0,0 +1,19 @@
+#include "raylib.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//--------------------------------------------------------------------------------- VARIÁVEIS ---------------------------------------------------------------------------
+
+
+#define LARGURA 680
+#define ALTURA 680 
+#define FPS 60 
+#define GRAVIDADE 0.5 
+#define POPULAÇÃO 250 
+#define ELITES 25 
+#define TAXA_DE_MUTAÇÃO 0.05 
+
+
+
