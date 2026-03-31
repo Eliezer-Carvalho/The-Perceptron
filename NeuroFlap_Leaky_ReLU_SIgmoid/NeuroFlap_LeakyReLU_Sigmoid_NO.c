5a3bd60393032785552930c038d1dee5c1dceab9
Eliezer <eliezer.carvalho.11.8a@gmail.com>
2026-03-31 19:50:31 +0100

    main

--- NeuroFlap_Leaky_ReLU_SIgmoid/NeuroFlap_LeakyReLU_Sigmoid_NO.c
+++ NeuroFlap_Leaky_ReLU_SIgmoid/NeuroFlap_LeakyReLU_Sigmoid_NO.c
@@ -75,7 +75,7 @@ int COMPARAÇÃO (const void *a, const void *b);
 
 
 double FUNÇÃO_ATIVAÇÃO_SIGMOID (double x);
-double FUNÇÃO_ATIVAÇÃO_ReLU (double x);
+double FUNÇÃO_ATIVAÇÃO_LEAKY_ReLU (double x);
 
 
 void RESET_JOGO (struct TUBOS colunas []);
@@ -264,20 +264,21 @@ int COMPARAÇÃO (const void *a, const void *b) {
 double FUNÇÃO_ATIVAÇÃO_SIGMOID (double x) {
 
     return 1.0 / (1.0 + exp(-x));
+
 }
 
 
-
-double FUNÇÃO_ATIVAÇÃO_ReLU (double x) {
+double FUNÇÃO_ATIVAÇÃO_LEAKY_ReLU (double x) {
 
     if (x < 0) {
+        
+        return x * 0.01;    
 
-        return 0;                
     }
 
     return x;
-}
 
+}
 
 
 
@@ -351,7 +352,7 @@ double MULTILAYER_PERCEPTRON (double INPUT1, double INPUT2, double INPUT3, doubl
                
         for (int j = 0; j < NÚMERO_NEURÓNIOS_CAMADA_OCULTA; j++) {
             
-            x -> OUTPUT_NEURÓNIO_HIDDEN_LAYER[j] = FUNÇÃO_ATIVAÇÃO_ReLU (x -> NEURÓNIO_HIDDEN_LAYER[j]);
+            x -> OUTPUT_NEURÓNIO_HIDDEN_LAYER[j] = FUNÇÃO_ATIVAÇÃO_LEAKY_ReLU (x -> NEURÓNIO_HIDDEN_LAYER[j]);
         
             }
         
